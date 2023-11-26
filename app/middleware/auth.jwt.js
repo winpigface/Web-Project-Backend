2c950abf04e8edcc9531e34428543211349b54b5
winpigface <winpigface2537@gmail.com>
2023-11-27 01:48:46 +0700

    auth

--- app/middleware/auth.jwt.js
+++ app/middleware/auth.jwt.js
@@ -2,9 +2,8 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const verifyToken = (req,res,next)=>{
     const token =req.headers["x-access-token"];
-    // const token = req.cookies.user_sid
     if(!token){
-        return res.status(403).send({message: "You have to login first"});
+        return res.status(401).send({message: "You have to login first"});
     }
     jwt.verify(token,process.env.Usertoken,(err,decoded)=>{
         if(err) return res.status(401).send({message: "Unauthorized"});
@@ -17,8 +16,6 @@ const verifyToken = (req,res,next)=>{
 };
 const verifytokenAdmin = (req,res,next)=>{
     const token =req.headers["x-access-token"];
-    // const token = req.cookies.user_sid
-
     if(!token){
         return res.status(403).send({message: "You have to login first"})
     }
