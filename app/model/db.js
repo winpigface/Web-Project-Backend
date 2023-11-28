9432c1d0d48464531adf1465bf826ba136f0c484
winpigface <winpigface2537@gmail.com>
2023-11-28 10:43:15 +0700

    bookModel

--- app/model/db.js
+++ app/model/db.js
@@ -3,19 +3,16 @@ const mysql = require("mysql2");
 require('dotenv').config()
 // const connection = mysql.createConnection(process.env.DB_URL)
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+// const connection = mysql.createConnection(process.env.DATABASE_URL)
 
-// var connection = mysql.createConnection({
-//     host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-//     port: 4000,
-//     user: '3cfbwgjaGCZtuWZ.root',
-//     password: 'S7Ktqvxjn6MPPiko',
-//     database: 'test',
-//     ssl: {
-//       minVersion: 'TLSv1.2',
-//       rejectUnauthorized: true
-//     }
-//   });
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'project',
+ 
+  });
 connection.connect((error)=>{ //check error
     if(error) console.log("MYsql connection " +error);//error : show error
     else console.log("Successfully connected to database");//not error : show success that can connnect to database
