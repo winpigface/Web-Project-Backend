2a0d453373676d05a214afadbc5fd72bce8053e7
winpigface <winpigface2537@gmail.com>
2024-02-05 14:38:28 +0700

    booktime

--- app/model/db.js
+++ app/model/db.js
@@ -11,7 +11,10 @@ require('dotenv').config()
 //   });
 var connection = mysql.createConnection(process.env.TiDB_URL)
 connection.connect((error)=>{ //check error
-    if(error) console.log("MYsql connection " +error);//error : show error
+    if(error){
+      console.log("MYsql connection " +error);//error : show error
+      connection.destroy();
+    } 
     else console.log("Successfully connected to database");//not error : show success that can connnect to database
 });
 module.exports = connection; 
